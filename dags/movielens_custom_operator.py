ec1485c8821de0beee4900c28a451d7bb043d051
ioannis.polyzos <ioannis.polyzos@metis.tech>
2020-07-05 19:19:03 +0300

    add custom resources + pod definition

---
+++ dags/movielens_custom_operator.py
@@ -0,0 +1,16 @@
+from airflow import DAG, utils as airflow_utils
+from custom.operators import MovielensFetchRatingsOperator
+
+with DAG(
+        dag_id="movielens_custom_operator",
+        description="Fetches ratings from the Movielens API using a custom operator.",
+        start_date=airflow_utils.dates.day_ago(7),
+        schedule_interval="@daily",
+) as DAG:
+    MovielensFetchRatingsOperator(
+        task_id="fetch_ratings",
+        conn_id="movielens",
+        start_date="{{ds}}",
+        end_date="{{next_ds}}",
+        output_path="/data/custom_operator/{{ds}}.json",
+    )
